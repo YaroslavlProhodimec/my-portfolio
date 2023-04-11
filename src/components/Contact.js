7ad974deb5c3f8afbaca345b8b66ab6179dea445
Yaroslav Myratov <yaroslav.muratov.97@inbox.ru>
2023-04-11 14:24:04 +0300

    the end verstki

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,55 @@
 import React from 'react';
 
+import {motion} from 'framer-motion';
+import {fadeIn} from '..//variants'
+
 const Contact = () => {
-  return <div className='section' id='contact'>Contact</div>;
+    return <section className='py-16 lg:section' id='contact'>
+        <div className="container mx-auto">
+            <div className='flex flex-col lg:flex-row'>
+                <motion.div variants={fadeIn('right', 0.3)}
+                            transition={{
+                              delay: 3,
+                              duration: 2,
+                              repeat: Infinity,
+                              repeatDelay: 15,
+                              repeatType: 'reverse',
+                              type: 'tween'
+                            }} className='flex-1 flex justify-start items-center'>
+                  <div>
+                    <h4 className=' text-x1 uppercase text-blue-600 font-medium mb-2 tracking-wide'>Get in touch</h4>
+                    <h2 className='text-[45px] lg:text-[90px] leading-none mb-12'>Let's work <br/> together!</h2>
+
+                    </div>
+                </motion.div>
+                <motion.form variants={fadeIn('left', 0.3)}
+                             transition={{
+                               delay: 3,
+                               duration: 2,
+                               repeat: Infinity,
+                               repeatDelay: 15,
+                               repeatType: 'reverse',
+                               type: 'tween'
+                             }} className='flex-1 border rounded-2xl flex flex-col gap-y-6 pb-24 p-6 items-start'>
+                    <input
+                        className='bg-transparent border-b py-3 outline-none w-full placeholder:text-white focus:border-blue transition-all '
+                        type='text'
+                        placeholder='Your name'
+                    /> <input
+                    className='bg-transparent border-b py-3 outline-none w-full placeholder:text-white focus:border-blue transition-all '
+                    type='text'
+                    placeholder='Your email'
+                />
+                  <textarea className='bg-transparent border-b py-3 outline-none w-full placeholder:text-white focus:border-blue transition-all
+                  resize-none mb-12
+                   '
+                            placeholder='Your message'
+                  ></textarea>
+                  <button className='btn btn-lg'>Send message</button>
+                </motion.form>
+            </div>
+        </div>
+    </section>;
 };
 
 export default Contact;
--- src/components/Project.js
+++ src/components/Project.js
@@ -5,45 +5,47 @@ import {fadeIn} from '..//variants'
 
 const services = [
     {
-        name: 'JAVA-SCRIPT',
-        description: 'lorem10',
+        name: 'Frontend Development ',
+        description: ' JAVA-SCRIPT, TYPE-SCRIPT',
         link: 'learn more',
     },
     {
-        name: 'TYPE-SCRIPT',
-        description: 'lorem10',
+        name: 'Design',
+        description: ' HTML, CSS, Tailwind, SCSS/SASS',
         link: 'learn more',
     },
     {
-        name: 'REACT',
-        description: 'lorem10',
+        name: 'Framework',
+        description: ' REACT',
         link: 'learn more',
     },
     {
-        name: 'STORYBOOK',
-        description: 'lorem10',
+        name: 'Tools',
+        description: ' GitHub, Storybook, Material-UI, Figma, Fhotoshop, Postman',
         link: 'learn more',
-    }
+    },
 ]
 const Project = () => {
     return <section className='section' id='services'>
         <div className='container mx-auto'>
             <div className='flex flex-col lg:flex-row'>
-                <div className='flex-1 lg:bg-services  lg:bg-bottom bg-no-repeat mb-12 lg:mb-0'><h2
+                <motion.div variants={fadeIn('right',0.3)} initial='hidden' whileInView={'show'}
+                            viewport={{once:false,amount:0.3}}  className='flex-1 lg:bg-services  lg:bg-bottom bg-no-repeat mb-12 lg:mb-0'><h2
                     className='h2 text-blue-600 mb-6'>My skills</h2>
                     {/*<h3 className='h3 '></h3>*/}
                     <button className='btn btn-sm'>See my work</button>
-                </div>
-                <div className='flex-1'>
+                </motion.div>
+                <motion.div variants={fadeIn('left',0.5)} initial='hidden' whileInView={'show'}
+                            viewport={{once:false,amount:0.3}} className=' flex-1'>
                     <div >
                         {services.map((service, index) => {
                             const {name, description, link} = service
                             return (
                                 <div className='border-b border-white/20 h-[146px] mb-[38px] flex' key={index}>
                                     <div className='max-w-[476px]'>
-                                        <h4 className='text-[20px] tracking-wider font-primary font-semibold mb-6'>{name}
+                                        <h4 className='font-secondary leading-tight mb-6'>{name}
                                     </h4>
-                                        <p className='font-secondary leading-tight '>{description}</p>
+                                        <p className='  text-[20px] tracking-wider font-primary font-semibold '>{description}</p>
                                     </div>
                                     <div className='flex flex-col flex-1 items-end'>
                                         <a href="#" className='btn w-9 h-9 mb-[42px] flex justify-center items-center'>
@@ -54,8 +56,9 @@ const Project = () => {
                                 </div>
                             )
                         })}
+
                     </div>
-                </div>
+                </motion.div>
             </div>
 
         </div>
